c2faddc750443675c9bf21629d941d000b6b616b
boroicamarius01@gmail.com <boroicamarius01@gmail.com>
2024-05-16 04:09:29 +0300

    changes on my side

---
+++ MazeGame/SceneView.swift
@@ -0,0 +1,18 @@
+//
+//  SceneView.swift
+//  MazeGame
+//
+//  Created by Marius Boroica on 16.05.2024.
+//
+
+import SwiftUI
+
+struct SceneView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SceneView()
+}
