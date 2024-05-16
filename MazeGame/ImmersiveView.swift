a6f8e4fc64e0f9a7f4f03fb1983bc7d3da9ecfb1
Razvi103 <bocrar@gmail.com>
2024-05-16 03:11:35 +0300

    Initial Commit

---
+++ MazeGame/ImmersiveView.swift
@@ -0,0 +1,25 @@
+//
+//  ImmersiveView.swift
+//  MazeGame
+//
+//  Created by Razvan Bocra on 16.05.2024.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ImmersiveView: View {
+    var body: some View {
+        RealityView { content in
+            // Add the initial RealityKit content
+            if let scene = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
+                content.add(scene)
+            }
+        }
+    }
+}
+
+#Preview(immersionStyle: .mixed) {
+    ImmersiveView()
+}
---
+++ MazeGame/MazeGameApp.swift
@@ -0,0 +1,21 @@
+//
+//  MazeGameApp.swift
+//  MazeGame
+//
+//  Created by Razvan Bocra on 16.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct MazeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.windowStyle(.volumetric)
+
+        ImmersiveSpace(id: "ImmersiveSpace") {
+            ImmersiveView()
+        }
+    }
+}
